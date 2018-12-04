6f5f60c3d136e6aaead79671226aeadba091a4a7
Jeremy Collette <jeremy.collette93@gmail.com>
2018-12-04 00:08:50 -0800

    Windows build_setup now copies include files and build copies assets

--- scripts/build_setup_windows.bat
+++ scripts/build_setup_windows.bat
@@ -3,55 +3,74 @@ SET midistar_dir=%cd%
 SET ext_dir=%cd%/external
 SET lib_dir=%cd%/lib
 SET dll_dir=%cd%/dll
+SET inc_dir=%cd%/include
 
-echo Resetting dll folder...
-rd /q /s dll
-mkdir dll
-
-echo.
-echo Resetting lib folder...
+echo Removing lib folder...
 rd /q /s lib
 mkdir lib
+
+echo.
+echo Resetting include folder...
+rd /q /s "%inc_dir%/CLI"
+rd /q /s "%inc_dir%/FluidSynth"
+rd /q /s "%inc_dir%/midifile"
+rd /q /s "%inc_dir%/rtmidi"
+rd /q /s "%inc_dir%/SFML"
+del "%inc_dir%/fluidsynth.h"
 
 echo.
 echo Setting up git submodules...
 git submodule init
 git submodule update
 
-echo Building fluidsynth...
+echo.
+echo Preparing CLI11...
+cd "%ext_dir%\CLI11" || goto :error
+xcopy /E "include\CLI" "%inc_dir%\CLI\" || goto :error
+
+echo.
+echo Preparing fluidsynth...
 cd "%ext_dir%/fluidsynth"
 mkdir build
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE="C:\Program Files\vcpkg\scripts\buildsystems\vcpkg.cmake" -Denable-pkgconfig:BOOL="0" || goto :error
 msbuild FluidSynth.sln || goto :error
 copy "src\Debug\*.lib" "%lib_dir%\." || goto :error
-copy "src\Debug\*.dll" "%dll_dir%\." || goto :error
+copy "src\Debug\*.dll" "%lib_dir%\." || goto :error
+xcopy /E "..\include\fluidsynth" "%inc_dir%\fluidsynth\" || goto :error
+copy "include\fluidsynth.h" "%inc_dir%\." || goto :error
+copy "include\fluidsynth\version.h" "%inc_dir%\fluidsynth\." || goto :error
 
 echo.
-echo Building midifile...
+echo Preparing midifile...
 cd "%ext_dir%/midifile" || goto :error
 mkdir build
 cd build
 cmake .. || goto :error
 msbuild midifile.sln || goto :error
 copy "Debug\*.lib" "%lib_dir%\." || goto :error
+mkdir "%inc_dir%\midifile" || goto :error
+copy "..\include\*.h" "%inc_dir%\midifile\." || goto :error
 
 echo.
-echo Building rtmidi...
+echo Preparing rtmidi...
 cd "%ext_dir%/rtmidi"
 copy "%midistar_dir%\scripts\rtmidi_makefile" Makefile || goto :error
 nmake /A rtmidi.lib || goto :error
 copy "*.lib" "%lib_dir%\." || goto :error
+mkdir "%inc_dir%\rtmidi" || goto :error
+copy "*.h" "%inc_dir%\rtmidi\." || goto :error
 
 echo.
-echo Building SFML...
+echo Preparing SFML...
 cd "%ext_dir%/SFML" || goto :error
 mkdir build
 cd build
 cmake .. || goto :error
 msbuild SFML.sln || goto :error
 copy "lib\Debug\*.lib" "%lib_dir%\." || goto :error
-copy "lib\Debug\*.dll" "%dll_dir%\." || goto :error
+copy "lib\Debug\*.dll" "%lib_dir%\." || goto :error
+xcopy /E "..\include\SFML" "%inc_dir%\SFML\" || goto :error
 
 echo.
 echo Finished! Refer to the README file for instructions on building midistar.

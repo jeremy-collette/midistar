22184f55586b4af2eb1a8d7782a3904573ba2468
Jeremy Collette <jeremy.collette93@gmail.com>
2019-03-09 23:03:09 -0800

    Build setup now finds VS using vswhere

--- scripts/win32/build_setup.bat
+++ scripts/win32/build_setup.bat
@@ -1,4 +1,6 @@
 @ECHO off
+SETLOCAL EnableDelayedExpansion
+
 REM Change to script dir:
 CD "%~dp0"
 
@@ -6,6 +8,7 @@ REM Change to source dir if we're in the scripts folder:
 FOR %%I in (.) do SET dir=%%~nxI
 IF "%dir%"=="win32" (CD ..\..)
 
+REM Setup variables:
 SET midistar_dir=%CD%
 SET ext_dir=%CD%\external
 SET lib_dir=%CD%\lib
@@ -15,6 +18,39 @@ SET dll_dir=%CD%\dll
 SET inc_dir=%CD%\include
 SET win_script_dir=%CD%\scripts\win32
 
+REM Find MSBuild:
+ECHO.
+ECHO Finding MSBuild.exe...
+FOR /f "usebackq tokens=*" %%i in (`%win_script_dir%\vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+  SET msbuild=%%i
+)
+IF "%msbuild%" == "" (
+    ECHO Could not find MSBuild! Please install Microsoft Developer Tools and try again.
+    GOTO :error
+) ELSE (
+    ECHO Found MSBuild at "%msbuild%".
+)
+
+REM Find Git:
+ECHO.
+ECHO Finding git...
+WHERE git
+if %errorLevel% == 1 (
+    ECHO Could not find git^^! Please install git, add it to the system PATH variable, re-open the terminal, and try again.
+    GOTO :error
+)
+
+REM Find CMake:
+ECHO.
+ECHO Finding cmake...
+WHERE cmake
+if %errorLevel% == 1 (
+    ECHO Could not find cmake^^! Please install cmake, add it to the system PATH variable, re-open the terminal, and try again.
+    GOTO :error
+)
+
+REM Remove artifacts from previous builds:
+ECHO.
 ECHO Removing build folder...
 RD /q /s build
 
@@ -34,18 +70,20 @@ RD /q /s "%inc_dir%\rtmidi"
 RD /q /s "%inc_dir%\SFML"
 DEL "%inc_dir%\fluidsynth.h"
 
+REM Setup git submodules:
 ECHO.
 ECHO Setting up git submodules...
 git submodule init
 git submodule update
 
+REM Setup pre-requisites:
 ECHO.
 ECHO Preparing vcpkg...
 CD "%ext_dir%\vcpkg" || GOTO :error
 git clean -fdx
 CALL bootstrap-vcpkg.bat
 IF NOT %errorlevel%==0 (
-	GOTO :error
+    GOTO :error
 )
 vcpkg install glib || GOTO :error
 
@@ -62,10 +100,10 @@ git clean -fdx
 MKDIR build
 CD build
 cmake .. -DCMAKE_TOOLCHAIN_FILE="%ext_dir%\vcpkg\scripts\buildsystems\vcpkg.cmake" -Denable-pkgconfig:BOOL="0" || GOTO :error
-msbuild FluidSynth.sln /p:Configuration=Debug || GOTO :error
+"%msbuild%" FluidSynth.sln /p:Configuration=Debug || GOTO :error
 COPY "src\Debug\*.lib" "%lib_dir_debug%\." || GOTO :error
 COPY "src\Debug\*.dll" "%lib_dir_debug%\." || GOTO :error
-msbuild FluidSynth.sln /p:Configuration=Release || GOTO :error
+"%msbuild%" FluidSynth.sln /p:Configuration=Release || GOTO :error
 COPY "src\Release\*.lib" "%lib_dir_release%\." || GOTO :error
 COPY "src\Release\*.dll" "%lib_dir_release%\." || GOTO :error
 XCOPY /E "..\include\fluidsynth" "%inc_dir%\fluidsynth\" || GOTO :error
@@ -79,9 +117,9 @@ git clean -fdx
 MKDIR build
 CD build
 cmake .. || GOTO :error
-msbuild midifile.sln /p:Configuration=Debug || GOTO :error
+"%msbuild%" midifile.sln /p:Configuration=Debug || GOTO :error
 COPY "Debug\*.lib" "%lib_dir_debug%\." || GOTO :error
-msbuild midifile.sln /p:Configuration=Release || GOTO :error
+"%msbuild%" midifile.sln /p:Configuration=Release || GOTO :error
 COPY "Release\*.lib" "%lib_dir_release%\." || GOTO :error
 MKDIR "%inc_dir%\midifile" || GOTO :error
 COPY "..\include\*.h" "%inc_dir%\midifile\." || GOTO :error
@@ -106,10 +144,10 @@ git clean -fdx
 MKDIR build
 CD build
 cmake .. || GOTO :error
-msbuild SFML.sln /p:Configuration=Debug || GOTO :error
+"%msbuild%" SFML.sln /p:Configuration=Debug || GOTO :error
 COPY "lib\Debug\*.lib" "%lib_dir_debug%\." || GOTO :error
 COPY "lib\Debug\*.dll" "%lib_dir_debug%\." || GOTO :error
-msbuild SFML.sln /p:Configuration=Release || GOTO :error
+"%msbuild%" SFML.sln /p:Configuration=Release || GOTO :error
 COPY "lib\Release\*.lib" "%lib_dir_release%\." || GOTO :error
 COPY "lib\Release\*.dll" "%lib_dir_release%\." || GOTO :error
 XCOPY /E "..\include\SFML" "%inc_dir%\SFML\" || GOTO :error

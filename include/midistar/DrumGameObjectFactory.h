31013a117f42554ae3a60bc8431cfb2abbab07a1
Jeremy Collette <jeremy.collette93@gmail.com>
2021-04-04 09:56:17 -0700

    Refactoring

--- include/midistar/DrumGameObjectFactory.h
+++ include/midistar/DrumGameObjectFactory.h
@@ -92,7 +92,7 @@ class DrumGameObjectFactory : public GameObjectFactory {
     static const sf::Color DRUM_COLOURS[NUM_DRUM_COLOURS];  //!< Holds drum
                                                                   //!< colours
 
-    GameObject* DrumGameObjectFactory::CreatePhantomInstrument(
+    GameObject* DrumGameObjectFactory::CreatePhantomInstrumentNotes(
         std::set<int> midi_keys);  //!< Handles MIDI input for non-visible drums
                         //!< (i.e. drums that are not used in the current song).
     GameObject* CreateInstrumentNote(int midi_key);  //!< Creates a note for

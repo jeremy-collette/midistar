36be8190936d2bb786860bf04d991c99c6fb7a19
Jeremy Collette <jeremy.collette93@gmail.com>
2021-11-29 21:07:59 -0800

    Build setup now copies SFML external libs to lib folder on OSX (#107)

--- include/midistar/DrumGameObjectFactory.h
+++ include/midistar/DrumGameObjectFactory.h
@@ -92,9 +92,9 @@ class DrumGameObjectFactory : public GameObjectFactory {
     static const sf::Color DRUM_COLOURS[NUM_DRUM_COLOURS];  //!< Holds drum
                                                                   //!< colours
 
-    GameObject* DrumGameObjectFactory::CreatePhantomInstrumentNotes(
-        std::set<int> midi_keys);  //!< Handles MIDI input for non-visible drums
-                        //!< (i.e. drums that are not used in the current song).
+    GameObject* CreatePhantomInstrumentNotes(std::set<int> midi_keys);
+      //!< Handles MIDI input for non-visible drums (i.e. drums that are not
+                                                //!< used in the current song).
     GameObject* CreateInstrumentNote(int midi_key);  //!< Creates a note for
                                  //!< the instrument with the given MIDI key
     void GetInstrumentKeyBinding(int midi_key, sf::Keyboard::Key* key,
